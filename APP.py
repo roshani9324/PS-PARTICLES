c78b4cb7845f27722d3ed203d2c945e3716ffbf8
roshanimaurya.in@gmail.com <avasheshmaurya419@gmail.com>
2024-08-08 01:34:58 +0530

    first commit

---
+++ APP.py
@@ -0,0 +1,15 @@
+
+from flask import Flask, request, render_template
+app Flask (_name_)
+@app.route('/register', methods=['GET', 'POST'])
+
+def register():
+    if request.method 'POST':
+        name request.form["name"]
+        email request.form['email']
+        password request.form['password"]
+                              return render_template('success.html')
+                              return render_template('register.html')
+if __name__=='__main__':
+app.run(host '0.0.0.0', port-5000)
+#http://localhost: 5000/register
